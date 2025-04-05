f28bf9502cc54e32f3e3fc3912c407dbaf581b01
louisprvst <louis.prevost1610@gmail.com>
2025-04-05 10:06:36 +0200

    V1 de la doc de l'api

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,19 @@ import { coureur_router } from './Coureurs/coureur_router';
 import { etapes_router } from './Etapes/etapes_router';
 import { equipes_router } from './Equipes/equipes_router';
 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
+
 const app = express();
 const port = 11048;
 
 app.use(express.json());
+
+// Route vers la doc de l'api
+const swaggerDocument = YAML.load(path.join(__dirname, './../swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Utiliser le routeur pour les utilisateurs
 app.use('/user', user_router);
